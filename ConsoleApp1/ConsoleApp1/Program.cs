6f318620f6fb6a9b93986be4117a1b84a2b2d200
Xertyz <pertyz911@gmail.com>
2023-09-20 13:25:24 +0300

    commit1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class Calculator
+{
+    static void Main()
+    {
+        double num1, num2;
+        char operation;
+
+        Console.WriteLine("Simple Console Calculator");
+        Console.WriteLine("Available operations: +, -, *, /");
+
+        Console.Write("Input first number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Input operand: ");
+        operation = Console.ReadLine()[0];
+
+        Console.Write("Input second number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+
+        Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
+    }
+}
